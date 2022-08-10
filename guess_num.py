46acaa382772675b26839bfbfcd66a2ea1c56b10
joechien29 <76095375+joechien29@users.noreply.github.com>
2022-08-10 17:42:21 +0800

    add count

--- guess_num.py
+++ guess_num.py
@@ -1,33 +1,32 @@
-# 產生一個隨機整數1~100
+# 產生一個隨機變數，起始值/結束值讓使用者去設定
 # 讓使用者重複輸入數字去猜
+# 計算猜的次數
 # 猜對的話，印出"猜對了"
 # 猜錯的話，要告訴他比答案大/小
 
 
 import random
 
-r = random.randint(1, 100)
-guess_int = input("輸入你猜的數字")
-guess_int = int(guess_int)
-print("隨機出來的答案是: ",r)
+start = input("請輸入隨機數字的起始值: ")
+end = input("請輸入隨機數字的結束值: ")
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+print("***隨機出來的答案是: ", r, "***")
 
-i = 10
-
-while i > 0:
-    i = i - 1
+count = 0 # 請算猜的次數
+while True:
+    count += 1
+    guess_int = input("請輸入你猜的數字: ")
+    guess_int = int(guess_int)
     if guess_int == r:
         print("猜對了")
+        print("你已經猜了", count, "次")
         break
-    else:
-        if i > 0:
-            print("你還有", i, "次機會")
-            guess_int = input("請輸入你猜的數字: ")
-            guess_int = int(guess_int)
-            if guess_int > r:
-                print("你的數字比答案大")
-            elif guess_int < r:
-                print("你的數字比答案小")
-        else:
-            print("遊戲結束")
+    elif guess_int > r:
+        print("你的數字比答案大")
+    elif guess_int < r:
+        print("你的數字比答案小")
+    print("你已經猜了", count, "次")
             
        
